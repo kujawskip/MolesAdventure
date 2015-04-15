163b204d11e38247d4c8177d729160d1335cf034
254201 kujawski patryk <patryk.kujawski93@gmail.com>
2015-04-15 09:10:09 +0200

    Implementing methods in Generic Layer

--- MolesAdventure/Generic XNA Layer/Objects/HighScores.cs
+++ MolesAdventure/Generic XNA Layer/Objects/HighScores.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Runtime.Serialization;
 namespace Generic_Game_Engine.Objects
 {
+	[Serializable]
     public class HighScores
     {
+ 	public	List<Tuple<string, int>> Scores {get; private set; }
         public HighScores(List<Tuple<string, int>> Scores)
         {
+		this.Scores=Scores;	
         }
         public void Add(Tuple<string, int> score)
         {
+		Scores.Add(score);
         }
         public void Save(string path)
         {
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
+		formatter.Serialize(stream, this);
+		stream.Close();
         }
         public static HighScores LoadHighscores(string path)
         {
-            return null;
+           	IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+		object obj = formatter.Deserialize(stream);
+		stream.Close();
+		return (HighScores)obj;
+ 
         }
     }
 }
